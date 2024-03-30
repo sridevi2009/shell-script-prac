3c7b42e60a3ca99d8a35af33a1cacfa0fa69ede0
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2024-03-30 17:58:38 +0530

    shell-script

---
+++ o4-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+Date=$(date)
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+
+echo "Script Execution Time is: ${Date}"
+echo "PERSON1: HI PERSON3, Hw r u"
+echo "PERSON2: HI PERSON1, how are you"
+echo "PERSON3: Hi PERSON1,PERSON2, very good morning all"
+echo "PERSON1: Very good morning PERSON2 and PERSON3" 
