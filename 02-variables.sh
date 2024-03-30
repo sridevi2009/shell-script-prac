d9e5f681b7f4a992ab2e56d8dc6939f6cd5769fc
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2024-03-30 17:39:51 +0530

    shell-script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-Person1=Ramesh
-Person2=suresh
+Person1=Ram
+Person2=rahim
 echo "$Person1: Hello $Person2, good morning"
 echo "$Person2: Hi $Person1, very good morning"
 echo "$Person1: How are you $Person2"
