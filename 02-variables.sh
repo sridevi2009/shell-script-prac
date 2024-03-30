23536a781f2557098b5282e70351f4e15b648817
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2024-03-30 17:52:00 +0530

    shell-script

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,6 +2,7 @@
 Date: $(date)
 Person1=Ram
 Person2=Rahim
+echo "Script Execution Time is: ${Date}"
 echo "$Person1: Hello $Person2, good morning"
 echo "$Person2: Hi $Person1, very good morning"
 echo "$Person1: How are you $Person2"
