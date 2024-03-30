22e810663185fcb8715579a3817c0397b73c31aa
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2024-03-30 18:51:35 +0530

    shell-script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -4,3 +4,8 @@ Fruits=("apple" "banana" "grapes")
 echo "first value is: ${Fruits[0]}"
 echo "second value is: ${Fruits[1]}"
 echo "third value is: ${Fruits[2]}"
+
+# value start from 0,1,2
+# not from 1,2,3
+
+echo "all fruits list is: $Fruits"
