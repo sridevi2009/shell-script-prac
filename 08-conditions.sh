07b02c0b5f396083694e9e9e905ab9848e4c27a8
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2024-03-30 19:12:59 +0530

    shell-script

---
+++ 08-conditions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#example problem
+#if(today != "sunday") {
+#     print "go to school"
+# }
+# else {
+#     print " happy Holiday"
+# }
+
+# 20
+# if 20 is greater than 100 then print, given number is greater than 20 otherwise print less than 100
+# $Number > 100
+# $Number -gt 100
+# $Number -lt 100
+# $Number -ge 100
+# $Number -le 100
+
+
+Number=$1
+if ( $Number -gt 100 ) {
+    print "given $Number is greater than 100"
+}
+else {
+    print "given $Number is less than 100"
+} 
