f888a50875ba80fb1047995ed8378e914253bb39
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2024-03-30 18:18:26 +0530

    shell-script

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Please enter your username:"
-read USERNAME
-echo "Username is: ${USERNAME}"
+read -s USERNAME
+echo "Username is: $USERNAME"
 echo "Please enter your Password:"
-read PASSWORD
+read -s PASSWORD
 echo "Password is: $PASSWORD"
 # echo "Username: $Username, Password is: $Password" u can use both with braces pr without brackets
