e9d1a0fd89311e65db9742e998b80e05bebd8350
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2024-04-01 15:41:43 +0530

    shell

--- 15-delete-old-logs.sh
+++ 15-delete-old-logs.sh
@@ -22,7 +22,7 @@
 # find . -type f -mtime +14 -name "*.log"
 
 #!/bin/bash
-SOURCE_DIR="/tmp/shellscript-logs"
+SOURCE_DIR=/tmp/shellscript-logs
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -38,6 +38,7 @@ FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 while IFS= read -r line
 do
     echo "Deleting file: $line"
+    rm -rf $line
 done <<< $FILES_TO_DELETE
 
 
