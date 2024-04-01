4dd76ee7bed40ee0610a8a310aa801a9ad8e4479
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2024-04-01 16:57:52 +0530

    shell

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -35,9 +35,9 @@ do
     partition=$(echo $line | awk '{print $1F}')
     if [ $usage -ge $DISK_THRESHOLD ]
     then
-        message+="High Disk Usage on $partition: $usage"
+        message+="High Disk Usage on $partition: $usage\n"
     fi   
 
 done  <<< $DISK_USAGE
 
-echo "message: $message"
+echo -e "message: $message"
